14b8fb4a9e4b7baa3c3a00132661610fa4f6d919
werfa1 <pasha.otverchenko@gmail.com>
2021-06-15 17:38:58 +0300

    Need to fix the constraints problem

--- User Interface iOS/ViewController.swift
+++ User Interface iOS/ViewController.swift
@@ -13,7 +13,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        let loginView = UIView()
+        loginView.frame.size = CGSize(width: view.frame.size.width / 2, height: view.frame.size.height / 4)
+        loginView.center = view.center
+        loginView.backgroundColor = .purple
+        
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            loginView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+        ])
+    }
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.willTransition(to: newCollection, with: coordinator)
+        view.layoutIfNeeded()
+        
+    }
 
 }
 
