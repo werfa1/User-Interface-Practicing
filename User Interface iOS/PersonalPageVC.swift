c135cf5a77f340da86e72a41a2a0e8f6b8e7c0e3
werfa1 <pasha.otverchenko@gmail.com>
2021-06-22 20:05:25 +0300

    Added Name label to personal page

--- User Interface iOS/PersonalPageVC.swift
+++ User Interface iOS/PersonalPageVC.swift
@@ -21,20 +21,21 @@ class PersonalPageVC: UIViewController {
         super.viewWillAppear(true)
         view.backgroundColor = .white
         configureProfilePic()
+        configurePersonalName()
     }
     
     //MARK: - Functions
     
     private func configureProfilePic () {
         
-        let outerView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        outerView.clipsToBounds = false
-        outerView.layer.shadowColor = UIColor.black.cgColor
-        outerView.layer.shadowOpacity = 1
-        outerView.layer.shadowOffset = CGSize.zero
-        outerView.layer.shadowRadius = 10
-        outerView.layer.shadowPath = UIBezierPath(roundedRect: outerView.bounds, cornerRadius: outerView.frame.size.width / 2).cgPath
-//        outerView.layer.cornerRadius = outerView.frame.size.width / 2
+        /// Container view to make shadow visible
+        let outerView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width / 3, height: view.frame.size.width / 3))
+        outerView.clipsToBounds         = false
+        outerView.layer.shadowColor     = UIColor.black.cgColor
+        outerView.layer.shadowOpacity   = 1
+        outerView.layer.shadowOffset    = CGSize.zero
+        outerView.layer.shadowRadius    = 10
+        outerView.layer.shadowPath      = UIBezierPath(roundedRect: outerView.bounds, cornerRadius: outerView.frame.size.width / 2).cgPath
         
         view.addSubview(outerView)
         
@@ -46,35 +47,36 @@ class PersonalPageVC: UIViewController {
             outerView.heightAnchor.constraint(equalTo: outerView.widthAnchor)
         ])
         
+        // Profile picture itself
         profilePicImageView.backgroundColor = .white
-        profilePicImageView.contentMode = .scaleAspectFill
-        profilePicImageView.frame = outerView.frame
-        profilePicImageView.image = UIImage(named: "rock")
+        profilePicImageView.contentMode     = .scaleAspectFill
+        profilePicImageView.frame           = outerView.frame
+        profilePicImageView.image           = UIImage(named: "rock")
         outerView.addSubview(profilePicImageView)
 
-//        profilePicImageView.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            profilePicImageView.leadingAnchor.constraint(equalTo: outerView.leadingAnchor),
-//            profilePicImageView.topAnchor.constraint(equalTo: outerView.topAnchor),
-//            profilePicImageView.trailingAnchor.constraint(equalTo: outerView.trailingAnchor),
-//            profilePicImageView.bottomAnchor.constraint(equalTo: outerView.bottomAnchor)
-//        ])
-
         //Making imageView circular
-        profilePicImageView.layer.borderWidth = 5.0
+        profilePicImageView.layer.borderWidth   = 5.0
         profilePicImageView.layer.masksToBounds = false
-        profilePicImageView.layer.borderColor = UIColor.white.cgColor
-        profilePicImageView.layer.cornerRadius = outerView.frame.size.width / 2 //This divider should be 0.5 of widthAnchor constraint divider
-        profilePicImageView.clipsToBounds = true
-        profilePicImageView.layer.shadowRadius = 5
-        profilePicImageView.layer.shadowColor = UIColor.black.cgColor
-        
-        
-        
+        profilePicImageView.layer.borderColor   = UIColor.white.cgColor
+        profilePicImageView.layer.cornerRadius  = outerView.frame.size.width / 2 //This divider should be 0.5 of widthAnchor constraint divider
+        profilePicImageView.clipsToBounds       = true
+        profilePicImageView.layer.shadowRadius  = 5
+        profilePicImageView.layer.shadowColor   = UIColor.black.cgColor
     }
     
     private func configurePersonalName () {
-        personalNameLabel.text = "The Greatest"
+        personalNameLabel.text                      = "Dwayne 'The Rock' Johnson"
+        personalNameLabel.numberOfLines             = 0
+        personalNameLabel.adjustsFontSizeToFitWidth = true
+        personalNameLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        view.addSubview(personalNameLabel)
+        
+        personalNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            personalNameLabel.leadingAnchor.constraint(equalTo: (profilePicImageView.superview?.trailingAnchor)!, constant: 16),
+            personalNameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            personalNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            personalNameLabel.heightAnchor.constraint(equalToConstant: 75)
+        ])
     }
-
 }
